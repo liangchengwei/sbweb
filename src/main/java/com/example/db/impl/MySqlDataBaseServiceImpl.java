f57280db520b8e5481f7ffd535565851bb9ea80f
liangchengwei <15910856677@139.com>
2018-08-09 11:17:56 +0800

    新增

---
+++ src/main/java/com/example/db/impl/MySqlDataBaseServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.db.impl;
+
+import com.example.db.DataBaseServer;
+
+import javax.xml.crypto.Data;
+import java.util.List;
+
+public class MySqlDataBaseServiceImpl implements DataBaseServer {
+    @Override
+    public List getData() {
+        return null;
+    }
+}
